516ee71084ecc971f7c1b653bc3c5fb7507e2564
Nikhil-Fexle-Training <nikhil.kumawat@fexle.com>
2022-09-02 20:00:21 +0530

    wire-commit

---
+++ SRC/lwc/dayTwo/dayTwo.js
@@ -0,0 +1,56 @@
+import { LightningElement, wire } from 'lwc';
+import methodOne from '@salesforce/apex/methodList.getTextMethod1';
+import methodTwo from '@salesforce/apex/methodList.getTextMethod2';
+import methodThree from '@salesforce/apex/methodList.getTextMethod3';
+
+export default class DayTwo extends LightningElement {
+    @wire(methodOne) statusOne;
+    @wire(methodTwo) statusTwo;
+    @wire(methodThree) statusThree;
+
+    // handleLoad(){
+    handleLoadOne(){
+        methodOne()
+            .then(result => {
+                this.statusOne = result;
+            })
+            .catch(error => {
+                this.error = error;
+            });
+    }
+
+    // handleLoad(){
+    handleLoadTwo(){
+        methodOne()
+            .then(result => {
+                this.statusOne = result;
+            })
+        methodTwo()
+            .then(result => {
+                this.statusTwo = result;
+            })
+            .catch(error => {
+                this.error = error;
+            });
+    }
+
+    // handleLoad(){
+    handleLoadThree(){
+        methodOne()
+            .then(result => {
+                this.statusOne = result;
+            })
+        methodTwo()
+            .then(result => {
+                this.statusTwo = result;
+            })
+        methodThree()
+            .then(result => {
+                this.statusThree = result;
+            })
+            .catch(error => {
+                this.error = error;
+            });
+    }
+    
+}
